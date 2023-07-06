dcc5dfbc99d6a31f864bb1150c711aff2d83851f
Hung-Jin <arcspatio@gmail.com>
2023-07-06 17:32:43 -0400

    balance sheet development

---
+++ financial_etl/balance_sheet.py
@@ -0,0 +1,53 @@
+import os
+from yahooquery import Ticker
+from financial_etl.base import Base_ETL
+import pandas as pd
+
+class BalanceSheet_ETL(Base_ETL):
+
+    def __init__(self):
+        super().__init__()
+        self._username = os.getenv('YFINANCE_USER')
+        self._password = os.getenv('YFINANCE_PASSWORD')
+
+    def extract(self, symbols, filename_out=None):
+        '''Extract Income Statement with Yfianace API
+
+            Args: 
+                symbols = ['JPM', 'GS', 'MS', 'SIVBQ']
+            
+            Returns:
+                pd.DataFrame
+        '''
+
+        raw_data = Ticker(symbols=symbols, username=self._username, password=self._password).p_balance_sheet(trailing=False, frequency='q')
+
+        if filename_out is None:
+            if not os.path.exists(self.dir_data_lake):
+                os.makedirs(self.dir_data_lake)
+            
+            filename_out = os.path.join(self.dir_data_lake, 'balance_sheet.csv')
+
+        raw_data.to_csv(filename_out, index=True)
+    
+    def _rename_columns(self, df, column_rename_mapping={'asOfDate': 'date'}):
+        return df.rename(columns=column_rename_mapping)
+    
+    def _filter_by_date_range(self, df, date_range):
+        df['date'] = pd.to_datetime(df['date'])
+        df = df.loc[df['date'].isin(pd.date_range(start=date_range[0], end=date_range[1]))]
+        return df.reset_index(drop=True)
+    
+    def transform(self, filename_in, filename_out, drop_threshold=0.8):
+
+        df_raw = pd.read_csv(filename_in)
+
+        df = self._rename_columns(df_raw, column_rename_mapping={'asOfDate': 'date'})
+
+        df = self._filter_by_date_range(df, date_range=['2017-01-01', '2022-03-31'])
+        
+        df = self._filter_columns(df, drop_threshold)
+
+        df.to_csv(filename_out, index=False)
+
+        return df
---
+++ tests/test_balance_sheet.py
@@ -0,0 +1,39 @@
+import os
+import sys
+sys.path.append('../')
+
+from financial_etl.balance_sheet import BalanceSheet_ETL
+
+def test_extract(symbols, filename_out):
+
+    etl = BalanceSheet_ETL()
+
+    etl.extract(symbols, filename_out)
+
+def test_transform(filename_in, filename_out, drop_threshold=0.8):
+
+    etl = BalanceSheet_ETL()
+
+    df = etl.transform(filename_in, filename_out, drop_threshold)
+
+    return df
+
+
+if __name__ == '__main__':
+
+    dir_thisfile = os.path.dirname(os.path.abspath(__file__))
+    dir_repo = os.path.join(dir_thisfile, '../')
+    dir_data_lake = os.path.join(dir_repo, 'data_lake/')
+
+    dir_data = os.path.join(dir_repo, 'data')
+
+    if not os.path.exists(dir_data):
+        os.makedirs(dir_data)
+
+    symbols = ['JPM', 'GS']
+
+    filename_raw = os.path.join(dir_data_lake, 'balance_sheet.csv')
+    test_extract(symbols=symbols, filename_out=filename_raw)
+
+    filename_out = os.path.join(dir_data, 'balance_sheet.csv')
+    df = test_transform(filename_in=filename_raw, filename_out=filename_out, drop_threshold=0.8)
